4d8f52f87d13b5846c1d64d98857b2a46b7898ed
Matthias Büsing <it@buesing.de>
2024-12-13 18:34:42 +0100

    asdf

--- e2e/cypress/e2e/spec.cy.js
+++ e2e/cypress/e2e/spec.cy.js
@@ -4,9 +4,9 @@ describe('Newsletter', () => {
         cy.visit('https://www.jet.de/newsletter')
     });
 
-    // it('background color falsch', () => {
-    //     cy.get('body').should('have.css', 'background-color', 'rgb(12, 22, 33)')
-    // });
+    it('background color falsch', () => {
+        cy.get('body').should('have.css', 'background-color', 'rgb(12, 22, 33)')
+    });
 
     it('background color richtig', () => {
         cy.get('body').should('have.css', 'background-color', 'rgb(252, 248, 240)')
